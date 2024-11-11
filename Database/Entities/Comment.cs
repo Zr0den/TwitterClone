372fef734bea3e2e0b45582ff275938749a57203
Kristian <krazlo878@gmail.com>
2024-11-11 15:05:32 +0700

    ef

--- Database/Entities/Comment.cs
+++ Database/Entities/Comment.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TweetService.Entities
+namespace EF.Entities
 {
     public class Comment
     {
         string Content { get; set; }
+        [ForeignKey("User")]
         int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        [ForeignKey("Tweet")]
         int TweetId { get; set; }
+        public virtual Tweet Tweet { get; set; }
+
     }
 }
--- Database/Entities/User.cs
+++ Database/Entities/User.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UserService.Entities
+namespace EF.Entities
 {
     public class User
     {
