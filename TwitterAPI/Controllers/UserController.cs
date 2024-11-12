da93d99856b27b8fc114d76b6b3f6c658489600c
Zr0den <94046425+Zr0den@users.noreply.github.com>
2024-11-12 09:12:12 +0700

    CreateUser lavet fra API til Service

--- TwitterAPI/Controllers/UserController.cs
+++ TwitterAPI/Controllers/UserController.cs
@@ -1,46 +1,83 @@
 ﻿using Database.Entities;
 using Helpers;
+using MessageClient;
 using Microsoft.AspNetCore.Mvc;
-using TwitterAPI.Messaging;
 using UserProfileService;
 
 
 namespace TwitterAPI.Controllers
 {
+    internal static class MessageWaiter
+    {
+        public static async Task<T?>? WaitForMessage<T>(MessageClient<T> messageClient, string clientId, int timeout = 5000)
+        {
+            var tcs = new TaskCompletionSource<T?>();
+            var cancellationTokenSource = new CancellationTokenSource(timeout);
+            cancellationTokenSource.Token.Register(() => tcs.TrySetResult(default));
+
+            using (
+                var connection = messageClient.ListenUsingTopic<T>(message =>
+                {
+                    tcs.TrySetResult(message);
+                }, "User" + clientId, clientId)
+            )
+            {
+            }
+
+            return await tcs.Task;
+        }
+    }
+
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
         private readonly UserService _userService;
+        private readonly MessageClient<UserCreateDto> _createUserMessage;
+        private readonly MessageClient<UserProfileDto> _getUserMessage;
 
-        public UserController(UserService userService)
+        public UserController(UserService userService, MessageClient<UserCreateDto> createUserMessage, MessageClient<UserProfileDto> getUserMessage)
         {
             _userService = userService;
+            _createUserMessage = createUserMessage;
+            _getUserMessage = getUserMessage;
         }
         [HttpPost]
-        public async Task<ActionResult<UserProfileDto>> CreateUser(UserCreateDto userDto)
+        public async Task<ActionResult<string>> CreateUser(UserCreateDto userDto)
         {
-            var user = new User
+            _createUserMessage.SendUsingTopic(new UserCreateDto
             {
                 Name = userDto.Name,
-                UserTag = userDto.Username,
-                Email = userDto.Email,
+                Email = userDto.Email, 
                 Password = userDto.Password,
-                // Password handling should ideally involve hashing
-            };
+                UserTag = userDto.UserTag
+            }, "newUser");
 
-            await _userService.AddUserAsync(user);
+            
+            var response = await MessageWaiter.WaitForMessage(_getUserMessage, userDto.UserTag)!;
 
-            // Mapping the User entity to UserProfileDto
-            var result = new UserProfileDto
-            {
-                Id = user.Id,
-                Name = user.Name,
-                Username = user.UserTag,
-                Email = user.Email
-            };
+            return response != null ? response.Email : "Order timed out.";
+            //var user = new User
+            //{
+            //    Name = userDto.Name,
+            //    UserTag = userDto.Username,
+            //    Email = userDto.Email,
+            //    Password = userDto.Password,
+            //    // Password handling should ideally involve hashing
+            //};
 
-            return CreatedAtAction(nameof(_userService.GetUserByIdAsync), new { id = result.Id }, result);
+            //await _userService.AddUserAsync(user);
+
+            //// Mapping the User entity to UserProfileDto
+            //var result = new UserProfileDto
+            //{
+            //    Id = user.Id,
+            //    Name = user.Name,
+            //    Username = user.UserTag,
+            //    Email = user.Email
+            //};
+
+            //return CreatedAtAction(nameof(_userService.GetUserByIdAsync), new { id = result.Id }, result);
         }
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateUser(int id, UserUpdateDto userDto)
@@ -50,7 +87,7 @@ namespace TwitterAPI.Controllers
                 return NotFound();
 
             user.Name = userDto.Name;
-            user.UserTag = userDto.Username;
+            user.UserTag = userDto.UserTag;
             user.Email = userDto.Email;
 
             await _userService.UpdateUserAsync(user);
@@ -69,7 +106,7 @@ namespace TwitterAPI.Controllers
             {
                 Id = user.Id,
                 Name = user.Name,
-                Username = user.UserTag,
+                UserTag = user.UserTag,
                 Email = user.Email
             };
 
@@ -85,7 +122,7 @@ namespace TwitterAPI.Controllers
             {
                 Id = user.Id,
                 Name = user.Name,
-                Username = user.UserTag,
+                UserTag = user.UserTag,
                 Email = user.Email
             }).ToList();
 
